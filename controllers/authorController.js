27275523f23283f3945ad2f87210611d3752995c
Keenan Jones <keenanjones210@gmail.com>
2021-08-10 20:29:26 -0500

    Author updating

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -142,11 +142,50 @@ exports.author_delete_post = function(req, res, next) {
 };
 
 // Display Author update form on GET.
-exports.author_update_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author update GET');
+exports.author_update_get = function(req, res, next) {
+    async.parallel({
+        author: function(callback){
+            Author.findById(req.params.id).populate('author').exec(callback);
+        }
+    },
+    function(err, results){
+        if(err){return next(err)};
+        if(results.author == null){
+            const err = new Error('Author not found');
+            err.status = 404;
+            return next(err);
+        }
+        res.render('author_form', {
+            title: 'Update Author',
+            author: results.author
+        });
+    });
 };
 
 // Handle Author update on POST.
-exports.author_update_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author update POST');
-};
+exports.author_update_post = [
+    body('first_name', 'First Name must not be empty.').trim().isLength({min: 1}).escape(),
+    body('family_name', 'Family Name must not be empty.').trim().isLength({min: 1}).escape(),
+    body('date_of_birth.*').escape(),
+
+    (req, res, next) => {
+        const errors = validationResult(req);
+
+        var author = new Author({
+            first_name: req.body.first_name,
+            family_name: req.body.family_name,
+            date_of_birth: req.body.date_of_birth,
+            _id: req.params.id
+        });
+
+        if(!errors.isEmpty()){
+            res.render('author_form', {title: 'Update Author', author: author, errors: errors.array()});
+            return;
+        }else{
+            Author.findByIdAndUpdate(req.params.id, author, {}, function(err, theauthor){
+                if(err){return next(err)};
+                res.redirect(theauthor.url);
+            });
+        }
+    }
+];
